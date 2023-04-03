b4a0db4131a1cf53c6fde3640f327840a45b97d6
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-03 12:43:55 -0400

    Fix deletion

--- labs/lab9/resources/iit.js
+++ labs/lab9/resources/iit.js
@@ -1,8 +1,8 @@
 function validate(formObj) {
   
-  if (formObj.firstNames.value == "") {
+  if (formObj.title.value == "") {
     alert("Please enter a first name");
-    formObj.firstNames.focus();
+    formObj.title.focus();
     return false;
   }
   
@@ -22,27 +22,27 @@ function validate(formObj) {
 }
 
 
-$(document).ready(function() {
+function moviesOnLoad() {
   
   // focus the name field on first load of the page
-  $("#firstNames").focus();
+  $("#title").focus();
      
-  $(".deleteActor").click(function() {
-    if(confirm("Remove actor? (This action cannot be undone.)")) {
+  $(".deleteMovie").click(function() {
+    if(confirm("Remove movie? (This action cannot be undone.)")) {
       
       // get the id of the clicked element's row
       var curId = $(this).closest("tr").attr("id");
-      // Extract the db id of the actor from the dom id of the clicked element
-      var actorId = curId.substr(curId.indexOf("-")+1);
+      // Extract the db id of the movie from the dom id of the clicked element
+      var movieId = curId.substr(curId.indexOf("-")+1);
       // Build the data to send. 
-      var postData = "id=" + actorId;
+      var postData = "id=" + movieId;
       // we could also format this as json ... jQuery will (by default) 
       // convert it into a query string anyway, e.g. 
-      // var postData = { "id" : actorId };
+      // var postData = { "id" : movieId };
       
       $.ajax({
         type: "post",
-        url: "actor-delete.php",
+        url: "movie-delete.php",
         dataType: "json",
         data: postData,
         success: function(responseData, status){
@@ -59,10 +59,10 @@ $(document).ready(function() {
             $(".messages").hide();
             
             // populate the js message box and show it:
-            $("#jsMessages").html("<h4>Actor deleted</h4>").show();
+            $("#jsMessages").html("<h4>Movie deleted</h4>").show();
             
             // re-zebra the table
-            $("#actorTable tr").each(function(i){
+            $("#movieTable tr").each(function(i){
               if (i % 2 == 0) {
                 // we must compensate for the header row...
                 $(this).addClass("odd"); 
@@ -80,5 +80,72 @@ $(document).ready(function() {
       
     }
   });
+}
+
+function moviesOnLoad() {
   
+  // focus the name field on first load of the page
+  $("#title").focus();
+     
+  $(".deleteMovie").click(function() {
+    if(confirm("Remove movie? (This action cannot be undone.)")) {
+      
+      // get the id of the clicked element's row
+      var curId = $(this).closest("tr").attr("id");
+      // Extract the db id of the movie from the dom id of the clicked element
+      var movieId = curId.substr(curId.indexOf("-")+1);
+      // Build the data to send. 
+      var postData = "id=" + movieId;
+      // we could also format this as json ... jQuery will (by default) 
+      // convert it into a query string anyway, e.g. 
+      // var postData = { "id" : movieId };
+      
+      $.ajax({
+        type: "post",
+        url: "movie-delete.php",
+        dataType: "json",
+        data: postData,
+        success: function(responseData, status){
+          if (responseData.errors) {
+            alert(responseData.errno + " " + responseData.error);
+          } else {
+            // Uncomment the following line to see the repsonse message from the server
+            // alert(responseData.message);
+            
+            // remove the table row in which the image was clicked
+            $("#" + curId).closest("tr").remove();
+            
+            // if a php generated message box is up, hide it:
+            $(".messages").hide();
+            
+            // populate the js message box and show it:
+            $("#jsMessages").html("<h4>Movie deleted</h4>").show();
+            
+            // re-zebra the table
+            $("#movieTable tr").each(function(i){
+              if (i % 2 == 0) {
+                // we must compensate for the header row...
+                $(this).addClass("odd"); 
+              } else {
+                $(this).removeClass("odd");
+              }
+            });
+          }
+        },
+        error: function(msg) {
+          // there was a problem
+          alert(msg.status + " " + msg.statusText);
+        }
+      });
+      
+    }
+  });
+}
+
+$(document).ready(function () {
+  if (location.href.endsWith("movies.php")){
+    moviesOnLoad();
+  } else {
+    moviesOnLoad()
+  }
 });
