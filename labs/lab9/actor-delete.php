bc07ee3d835f2654b95d378c4970bdf621b3471e
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-03 12:46:11 -0400

    Add debugging

--- labs/lab9/actor-delete.php
+++ labs/lab9/actor-delete.php
@@ -1,4 +1,7 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
   /* Delete an actor */
   
   /* Create a new database connection object, passing in the host, username,
